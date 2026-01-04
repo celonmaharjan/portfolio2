b3d571fa0fe95d14654cbf43c74cc245073e8059
Celon Maharjan <celon.maharjan@worldlink.com.np>
2026-01-04 23:06:55 +0545

    Add GitHub Actions deployment to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? "/portfolio2/" : "/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
