ed06d2207433e5eebeeddd5dbf6f6c1cab6abc5c
Celon Maharjan <celon.maharjan@worldlink.com.np>
2026-01-04 22:59:56 +0545

    Add SPA routing support for GitHub Pages and Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio2/",
+  base: process.env.NODE_ENV === 'production' ? "/portfolio2/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
